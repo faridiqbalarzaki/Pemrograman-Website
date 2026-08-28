624050cb4f15c34f9cb5d0b80dcc48bba3d63389
faridiqbalarzaki <faridiqbalarzaki@gmail.com>
2026-08-28 11:52:17 +0700

    Menghapus file dan folder sisa agar sinkron dengan lokal

--- PemogramanWeb2026-main/kode-praktikum/jobsheet-12/includes/helpers.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Membungkus output data yang berasal dari input pengguna sebelum
-// dicetak ke HTML, untuk mencegah XSS (Cross-Site Scripting).
-function e($value)
-{
-    return htmlspecialchars((string) ($value ?? ''), ENT_QUOTES, 'UTF-8');
-}
